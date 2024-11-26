6541802e0f77659ebc95cdc5820715c2cf153138
Marcos <dev.marcosanjos@gmail.com>
2024-11-26 10:14:02 -0300

    feat: integrate react-router-dom and create Home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
 import Header from "./components/header/header.component"
 
 const App = () => {
   return (
-    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
