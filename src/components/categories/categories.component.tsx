64755b442cd8460b39e1dcb30ae851128cd7ffbc
Marcos <dev.marcosanjos@gmail.com>
2024-11-26 10:36:03 -0300

    feat: fetch categories from API and display in Home page

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,9 +1,29 @@
-import { useState } from 'react';
-import './categories.styles.css';
+import { useEffect, useState } from 'react';
+
 import Category from '../../types/categories.types';
+import { env } from 'process';
+
+import './categories.styles.css';
+import axios from 'axios';
+
 
 const Categories = () => {
     const [categories, setCategories] = useState<Category[]>([])
+
+    const feachCategories = async () => {
+        try {
+            const {data} = await axios.get(`${process.env.REACT_APP_API_URL}/api/categories`);
+            setCategories(data);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        feachCategories();
+    }, [])
+
     return (
         <div className="categories-container">
             <div className="categories-content">
